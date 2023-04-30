1f258d3ab2da86ebbb04ec73d3768b641712fd1b
Anton Asanov <mrstride1@gmail.com>
2023-04-30 11:33:50 +0300

    added Chydesnye massivi 2

---
+++ src/ChydesnyeMassivi2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ChydesnyeMassivi2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = {1, 2, 2, 3};
+        int k;
+        int quantity = 0;
+        while (true) {
+            System.out.println("Введите число: ");
+            if (sc.hasNextInt()) {
+                k = Integer.parseInt(sc.nextLine());
+                break;
+            } else System.out.println("Вы ввели не число!");
+            sc.nextLine();
+        }
+        sc.close();
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] == k) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+                quantity++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println("Количество искомых элементов: " + quantity);
+    }
+}
