9e1ae9cd3d58949bfd4cea99b6a8a5a2f88544f0
Anton Asanov <mrstride1@gmail.com>
2023-04-30 09:53:29 +0300

    updated Chydesnye massivi

--- src/ChydesnyeMassivi.java
+++ src/ChydesnyeMassivi.java
@@ -1,16 +1,16 @@
 
 public class ChydesnyeMassivi {
     public static void main(String[] args) {
-        // Немного изменённый массив (проверял иные входные условия)
-        int[] nums = {0, 2, 0, 5, 3, 0, 6, 12, 0};
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == 0) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+        int[] nums = {0, 2, 0, 5};
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
+        }
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
     }
