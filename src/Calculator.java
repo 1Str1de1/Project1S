f2faf53f5483e72cb22aacbe09c82124739b0a88
1Str1de1 <131746337+1Str1de1@users.noreply.github.com>
2023-07-07 13:40:17 +0300

    Updated Calculator class

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,43 +1,84 @@
 import java.util.Scanner;
 
 public class Calculator {
+
+    private static double result = 0;
+
+    public static double variable;
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        int firstNumber;
-        int secondNumber;
-        int result;
-        String operator;
-            System.out.println("Введите первое число: ");
-            if (scanner.hasNextInt()) {
-                firstNumber = Integer.parseInt(scanner.nextLine());
-            } else {
-                System.out.println("Вы ввели не число!");
-                return;
+        Scanner scannerDouble = new Scanner(System.in);
+
+        String command;
+
+        do {
+
+            System.out.println("Welcome to the calculator\n" +
+                    "Please select an option:\n" +
+                    "+: add to a number\n" +
+                    "-: subtract a number\n" +
+                    "*: multiply by a number\n" +
+                    "/: divide by a number\n" +
+                    "0: exit\n" +
+                    "The result is:\n" + result);
+
+            command = scanner.nextLine();
+
+            switch (command) {
+
+                case "+":
+
+
+
+                    System.out.println("Which number do tou want to add?");
+
+                    variable = scannerDouble.nextDouble();
+
+                    result += variable;
+
+                    break;
+
+                case "-":
+
+                    System.out.println("Which number do tou want to subtract?");
+
+                    variable = scannerDouble.nextDouble();
+
+                    result -= variable;
+
+                    break;
+
+                case "*":
+
+                    System.out.println("By which number do tou want to multiply?");
+
+                    variable = scannerDouble.nextDouble();
+
+                    result *= variable;
+
+                    break;
+
+                case "/":
+
+                    System.out.println("By which number do tou want to divide?");
+
+                    variable = scannerDouble.nextDouble();
+
+                    result /= variable;
+
+                    break;
+
+                case "0":
+                    break;
+
+                default:
+                    System.out.println("Command not recognized. Please try again!");
             }
-            System.out.println("Введите второе число: ");
-            if (scanner.hasNextInt()) {
-                secondNumber = Integer.parseInt(scanner.nextLine());
-            } else {
-                System.out.println("Вы ввели не число!");
-                return;
-            }
-            System.out.println("Введите оператор: ");
-            operator = scanner.nextLine();
-            if (operator.equals("+")) {
-                result = firstNumber + secondNumber;
-            }
-            else if (operator.equals("-")) {
-                result = firstNumber - secondNumber;
-            }
-            else if (operator.equals("*")) {
-                result = firstNumber * secondNumber;
-            }
-            else if (operator.equals("/")) {
-                result = firstNumber / secondNumber;
-            } else {
-                System.out.println("Такого оператора не предусмотрено/не существует!");
-                return;
-            }
-        System.out.println("Результат: " + result);
+
+        } while (!command.equals("0"));
+
+
     }
 }
